3d59b919096783089e9b503180e426fef60450ab
Rahul Jha <rahulkumarjha26@gmail.com>
2018-10-24 12:55:56 +0530

    Updates

---
+++ precache-manifest.4b49d5486472a9a9164b8d120b79ef10.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "5abefdf9e13fefd4a6a8",
+    "url": "/robofriends/static/js/main.5abefdf9.chunk.js"
+  },
+  {
+    "revision": "d446094dfb41e95319ef",
+    "url": "/robofriends/static/js/1.d446094d.chunk.js"
+  },
+  {
+    "revision": "5abefdf9e13fefd4a6a8",
+    "url": "/robofriends/static/css/main.2adf87d9.chunk.css"
+  },
+  {
+    "revision": "d446094dfb41e95319ef",
+    "url": "/robofriends/static/css/1.cde5c24b.chunk.css"
+  },
+  {
+    "revision": "1380b3b9ba2d49749d394ae13998766f",
+    "url": "/robofriends/index.html"
+  }
+];
